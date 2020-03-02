ba19b0a80af002fef7d48e1636ca8ab91a096c98
mrcory <33401433+mrcory@users.noreply.github.com>
2020-03-01 22:37:59 -0500

    Comment and default adjustments

--- config.h
+++ config.h
@@ -10,12 +10,14 @@
 
 //Temerature settings
 Temperature temp = {
-  90,  //Current Temp (Don't change)
-  0 ,  //Target Temp
-  2 ,  //Temp Range
+  95,  //Current Temp (Don't change)
+  60 ,  //Target Temp
+  6 ,  //Temp Range
   0 ,  //Mode (0:F or 1:C) (Not coded)
 };
 
-//This amount of time must pass before the heater is allowd to turn back on (in seconds)
-//Though this shouldn't be needed with the temp.offset, I wished to add it anyway.
+/*This amount of time must pass before the heater is allowd to turn back on (in seconds)
+* Though this shouldn't be needed with the temp.offset, I wished to add it anyway.
+* This is an anti-short cycle feature.
+ */
 int cooldownPeriod = 30;
