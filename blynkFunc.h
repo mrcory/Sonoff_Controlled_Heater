8d812edc0e284c65aa0f33a370bd09f865dde5b1
mrcory <33401433+mrcory@users.noreply.github.com>
2020-03-01 22:36:39 -0500

    Added more controls

--- blynkFunc.h
+++ blynkFunc.h
@@ -2,22 +2,42 @@
  * 
  */
 
-BLYNK_WRITE(V50) {
-  temp.cur = param.asInt(); //Get temp from other device
+
+void sendBlynk() { //Feedback to the Blynk app
+  Blynk.virtualWrite(V49,temp.cur);
+  Blynk.virtualWrite(V54,temp.target);
+  Blynk.virtualWrite(V56,temp.offset);
+  Blynk.virtualWrite(V58,cooldownPeriod);
+  
+}
+
+BLYNK_WRITE(V51) {
+  temp.cur = param.asFloat(); //Get temp from other device
 }
 
 BLYNK_WRITE(V59) {
-  if (param.asInt() != relayOveride) {
-    relayOveride = param.asInt();
-  }
+  relayOveride = param.asInt();
 }
 
-void sendBlynk() { //Feedback to the Blynk app
-  Blynk.virtualWrite(V51,temp.cur);
+BLYNK_WRITE(V60) {
+  relayControl = param.asInt();
+}
 
-  if (isFirstRun) {
-    Blynk.virtualWrite(V53,temp.target);
-    Blynk.virtualWrite(V55,temp.offset);
-    Blynk.virtualWrite(V57,cooldownPeriod);
+BLYNK_WRITE(V53) {
+  temp.target = param.asFloat();
+}
+
+BLYNK_WRITE(V55) {
+  temp.offset = param.asFloat();
+}
+
+BLYNK_WRITE(V57) {
+  cooldownPeriod = param.asInt();
+}
+
+BLYNK_WRITE(V21) {
+  if (param.asInt() == 1) {
+    configSave();
+    Blynk.virtualWrite(21,0);
   }
 }
